113abb3ad443f218255e7d157a825960c7b646f4
Djvmil <ndieme16@hotmail.com>
2022-02-16 12:05:28 +0000

    amelioration lib authentification utils

--- sweet-alert/build/generated/source/buildConfig/debug/com/djamil/SweetAlert/BuildConfig.java
+++ sweet-alert/build/generated/source/buildConfig/debug/com/djamil/SweetAlert/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.djamil.SweetAlert";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 2;
-  public static final String VERSION_NAME = "1.0.6-beta38";
+  public static final String VERSION_NAME = "1.0.7";
 }
